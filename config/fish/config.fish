14b010926d1cc5fd0a29dd335d8e5ac9c1b15f34
arozx <jcooke@felsted.org>
2024-02-08 17:53:11 +0000

    aliases, omf, vi keybinds

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,5 +1,18 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    xbindkeys
-    colorscript -r
+    neofetch --ascii_distro arch_small
 end
+
+## cmd alias
+alias python='python3'
+alias vim='nvim'
+
+# cd
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+
+# ls
+alias ls='ls --color=auto'
+alias l='ls -lah'
+
