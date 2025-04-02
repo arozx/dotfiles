f7f9b3396310c6df3ef024e2035121bfec8c6040
arozx <jcooke@felsted.org>
2025-04-02 17:56:39 +0100

    update tmux config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,18 +1,43 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-    neofetch --ascii_distro arch_small
+function fish_prompt -d "Write out the prompt"
+    # This shows up as USER@HOST /home/user/ >, with the directory colored
+    # $USER and $hostname are set by fish, so you can just use them
+    # instead of using `whoami` and `hostname`
+    printf '%s@%s %s%s%s > ' $USER $hostname \
+        (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
 end
 
-## cmd alias
-alias python='python3'
-alias vim='nvim'
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set fish_greeting
 
-# cd
-alias ...='cd ../..'
-alias ....='cd ../../..'
-alias .....='cd ../../../..'
+end
 
-# ls
-alias ls='ls --color=auto'
-alias l='ls -lah'
+starship init fish | source
+if test -f ~/.cache/ags/user/generated/terminal/sequences.txt
+    cat ~/.cache/ags/user/generated/terminal/sequences.txt
+end
 
+alias pamcan=powerpill
+
+alias l="ls -lah"
+
+alias v=nvim
+alias vi=nvim
+alias vim=nvim
+
+alias gs="git status"
+alias gpush="git push"
+alias gpull="git pull"
+alias gc="git commit"
+
+alias e=exit
+alias c=clear
+
+alias keygh="eval (ssh-agent -c) && ssh-add ~/.ssh/github"
+
+alias dev-workspace='/home/glitch/scripts/tmux/dev-workspace.fish'
+
+# Start SSH agent if not running
+status is-interactive; and not set -q SSH_AUTH_SOCK; and eval (ssh-agent -c)
+
+oh-my-posh init fish | source
